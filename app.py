2b6f094987122aae3e9fb3b27d98938cf63272bb
Ozzy Gomes <ozzygomes@outlook.com>
2023-03-04 16:43:54 -0300

    Finalização do codigo

--- app.py
+++ app.py
@@ -18,6 +18,8 @@ class AIAssistant(QWidget):
         self.setLayout(self.layout['main'])
 
         self.init_ui()
+        self.init_configure_signals()
+        self.init_set_default_settings()
 
     def init_ui(self):
         #add sub layout manager
@@ -32,10 +34,10 @@ class AIAssistant(QWidget):
         self.max_tokens = QSlider(Qt.Orientation.Horizontal, minimum=10, maximum=4000, singleStep=500, pageStep=500, value=100)
         self.temperature = QSlider(Qt.Orientation.Horizontal, minimum=0, maximum=200)
         self.presence_penalty = QSlider(Qt.Orientation.Horizontal, minimum=0, maximum=200)
-
+        
         #add buttons
         self.btn_submit = QPushButton('&Submit', clicked=self.submit)
-        self.tbn_reset = QPushButton('&Reset', clicked=self.reset_fields)
+        self.btn_reset = QPushButton('&Reset', clicked=self.reset_fields)
 
         #add status bar
         self.status = QStatusBar()
@@ -43,14 +45,95 @@ class AIAssistant(QWidget):
         #organize widgets
         #----------------
         #maximum token slider
-        self.max_tokens_values = QLabel('0.0')
+        self.max_token_value = QLabel('0.0')
         self.layout['slider_layout'] = QHBoxLayout()
-        self.layout['slider_layout'].addWidget(self.max_tokens_values)
+        self.layout['slider_layout'].addWidget(self.max_token_value)
         self.layout['slider_layout'].addWidget(self.max_tokens)
         self.layout['inputs'].addRow(QLabel('Max Token:'), self.layout['slider_layout'])
 
         #temperature slider
         self.temperature_value = QLabel('0.0')
+        self.layout['slider_layout2'] = QHBoxLayout()
+        self.layout['slider_layout2'].addWidget(self.temperature_value)
+        self.layout['slider_layout2'].addWidget(self.temperature)
+        self.layout['inputs'].addRow(QLabel('Temperature:'), self.layout['slider_layout2'])
+
+        #presence penalty slider
+        self.presence_penalty_value = QLabel('0.0')
+        self.layout['slider_layout3'] = QHBoxLayout()
+        self.layout['slider_layout3'].addWidget(self.presence_penalty_value)
+        self.layout['slider_layout3'].addWidget(self.presence_penalty)
+        self.layout['inputs'].addRow(QLabel('Presence penalty:'), self.layout['slider_layout3'])
+
+        # textboxs
+        self.layout['inputs'].addRow(QLabel('Prompt:'), self.prompt)
+        self.layout['inputs'].addRow(QLabel('Output:'), self.output)
+        self.layout['inputs'].setLabelAlignment(Qt.AlignmentFlag.AlignRight)
+
+        #buttons
+        self.layout['buttons'] = QHBoxLayout()
+        self.layout['main'].addLayout(self.layout['buttons'])
+        
+        self.layout['buttons'].addWidget(self.btn_submit)
+        self.layout['buttons'].addWidget(self.btn_reset)
+        self.layout['main'].addWidget(self.status)
+
+    def init_set_default_settings(self):
+        #token slider
+        self.max_tokens.setTickPosition(QSlider.TickPosition.TicksBelow)
+        self.max_tokens.setTickInterval(500)
+        self.max_tokens.setTracking(True)
+        self.max_token_value.setText('{0:,}'.format(self.max_tokens.value()))
+
+        #temperature slider
+        self.temperature.setTickPosition(QSlider.TickPosition.TicksBelow)
+        self.temperature.setTickInterval(10)
+        self.temperature.setTracking(True)
+        self.temperature_value.setText('{0:.2f}'.format(self.temperature.value()))
+
+        #presence_penalty slider
+        self.presence_penalty.setTickPosition(QSlider.TickPosition.TicksBelow)
+        self.presence_penalty.setTickInterval(10)
+        self.presence_penalty.setTracking(True)
+        self.presence_penalty_value.setText('{0:.2f}'.format(self.presence_penalty.value()))
+
+    def init_configure_signals(self):
+        self.max_tokens.valueChanged.connect(lambda: self.max_token_value.setText('{0: ,}'.format(self.max_tokens.value())))
+        self.temperature.valueChanged.connect(lambda: self.temperature_value.setText('{0: .2f}'.format(self.temperature.value() / 100)))
+        self.presence_penalty.valueChanged.connect(lambda: self.presence_penalty_value.setText('{0: .2f}'.format(self.presence_penalty.value() / 100)))
+
+
+    def reset_fields(self):
+        self.prompt.clear()
+        self.output.clear()
+        self.status.clearMessage()
+
+    def submit(self):
+        text_block = self.prompt.toPlainText()
+        if not text_block:
+            self.status.showMessage('Prompt is empty.')
+            return
+        else:
+            self.status.clearMessage()
+
+        self.output.clear()
+
+        temperature = float('{0:.2f}'.format(self.temperature.value() / 100))
+        presence_penalty = float('{0:.2f}'.format(self.presence_penalty.value() / 100))
+        try:
+            response = self.openai_playground.send_prompt_request(text_block.strip(), max_tokens=self.max_tokens.value(), temperature=temperature, presence_penalty=presence_penalty)
+            self.output.setPlainText(response.get('outputs').strip())
+            self.status.setStyleSheet('''
+                color: green;
+            ''')
+            self.status.showMessage('Token used: {0}'.format(response.get('total_tokens')))
+        except Exception as e:
+            self.status.setStyleSheet('''
+                color: red;
+            ''')
+            self.status.showMessage((str(e)))
+
+
 
 class TabManager(QTabWidget):
     #add customized signals
@@ -104,21 +187,61 @@ class AppWindow(QWidget):
             }
         ''')
         self.tab_index_tracker = 1
+
         self.layout = {}
 
         #set the windows layout manager 
         self.layout['main'] = QVBoxLayout()
         self.setLayout(self.layout['main'])
 
+        self.layout['main'].insertSpacing(0, 20)
+
         self.init_ui()
+        self.init_configure_signal()
+        self.init_menu()
 
     def init_ui(self):
         # add tab manager
         self.tab_manager = TabManager()
         self.layout['main'].addWidget(self.tab_manager)
 
-        self.tab_manager.addTab(AIAssistant(), 'Conversation #{0}'.format(self.tab_index_tracker))
+        self.tab_manager.addTab(AIAssistant(), 'Conversa #{0}'.format(self.tab_index_tracker))
+    
+    def init_menu(self):
+        self.menuBar = QMenuBar(self)
+        self.menuBar.setStyleSheet('''
+            * {
+                background-color:#f0f0f0;
+                color: black;
+            }
+            *::item:selected {
+                background-color:#e3e3e3;
+            }
 
+        ''')
+
+        fileMenu = QMenu('&File', self.menuBar)
+        saveOutputAction = fileMenu.addAction('&Save Output', self.save_output)
+        self.menuBar.addMenu(fileMenu)
+
+    def add_tab(self):
+        self.tab_index_tracker += 1
+        self.tab_manager.addTab(AIAssistant(), 'Conversa #{0}'.format(self.tab_index_tracker))
+
+    def init_configure_signal(self):
+        self.tab_manager.plusClicked.connect(self.add_tab)
+
+    def save_output(self):
+        active_tab = self.tab_manager.currentWidget()
+
+        prompt = active_tab.prompt.toPlainText()
+        output = active_tab.output.toPlainText()
+        timestamp = datetime.now().strftime('%y_%m_%d_%H%M%S')
+
+        s = 'Prompt: \n{0}\n\nOutput:\n{1}'.format(prompt, output).strip()
+
+        with open('{0}_Conversa.txt'.format(timestamp), 'w') as _f:
+            _f.write(s)
 
 if __name__ == "__main__":
     #load OpenAI KEY
